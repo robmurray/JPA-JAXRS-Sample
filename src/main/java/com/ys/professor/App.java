436d6b7eec49f3b345a4581054b4908966975ff0
rob <tebor@wildmonkeybytes.com>
2015-01-10 19:11:09 -0500

    init commit

---
+++ src/main/java/com/ys/professor/App.java
@@ -0,0 +1,13 @@
+package com.ys.professor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

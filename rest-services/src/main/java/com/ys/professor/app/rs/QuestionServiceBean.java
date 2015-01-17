89455d7ca0fcd1e5a3ef21bd21889756afc09cd5
rob <tebor@wildmonkeybytes.com>
2015-01-16 23:29:20 -0500

    added PUT, DELETE, POST

--- rest-services/src/main/java/com/ys/professor/app/rs/QuestionServiceBean.java
+++ rest-services/src/main/java/com/ys/professor/app/rs/QuestionServiceBean.java
@@ -6,12 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.GenericEntity;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -19,22 +15,53 @@ import java.util.List;
 @Component
 @Path("/question")
 public class QuestionServiceBean {
+
+    @Context
+    private UriInfo uriInfo;
+
     @Autowired
     private QuestionService questionService;
 
-    //public void setQuestionService(QuestionService questionService) {
-    //    this.questionService = questionService;
-    //}
-
     @GET
-    @Produces("application/json")
+    @Produces(MediaType.APPLICATION_JSON)
     public Response getQuestions() {
-
         Collection<Question> questions = questionService.retrieveAllQuestions();
         GenericEntity<Collection<Question>> collection = new GenericEntity<Collection<Question>>(questions){
         };
-
         return Response.ok(collection).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getQuestion(@PathParam("id") Long id) {
+        Question question = questionService.retrieveQuestion(id);
+        return Response.ok(question).build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createQuestion(Question question){
+        //curl -i -H "Content-Type:application/json" -H "Accept: application/json" -X POST -d '{"description":"what is your name?","questionId":1}' http://localhost:9090/prof-rs/rs/question
+        return Response.ok().header ("Location", this.uriInfo.getAbsolutePathBuilder().path("1")).build();
 
     }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response updateQuestion(Question question){
+        //curl -i -H "Content-Type:application/json" -H "Accept: application/json" -X PUT -d '{"description":"what is your name?","questionId":1}' http://localhost:9090/prof-rs/rs/question
+        return Response.ok().build();
+    }
+
+    @Path("/{id}")
+    @DELETE
+    public Response deleteQuestion(@PathParam("id") Long id){
+        //curl -i -X DELETE http://localhost:9090/prof-rs/rs/question/1
+
+        return Response.ok().build();
+    }
+
 }
